606571f456e52b72a0d9762bd5403537328aa8f2
Needle <elena25-2025@ya.ru>
2018-11-04 14:53:46 +0300

    egg

---
+++ homeworks/lesson3/easter-egg/js/js.js
@@ -0,0 +1,33 @@
+'use strict';
+var menu = document.getElementsByTagName('nav')[0];
+var vis = document.getElementsByClassName('secret')[0];
+
+function open(ev) {
+  if(ev.ctrlKey && ev.altKey && ev.code === 'KeyT') {
+    console.log(menu.classList.contains('visible'));
+    ev.preventDefault();
+    menu.classList.toggle('visible');
+ }
+};
+
+document.addEventListener('keydown', open);
+document.addEventListener('keypress', open);
+document.addEventListener('keydown', secret);
+
+
+var cod = [];
+function secret(ev) {
+ cod.push(ev.code);
+ var enter = cod.join('');
+
+  let sec = ['KeyY','KeyT', 'KeyN', 'KeyJ', 'KeyK', 'KeyJ', 'KeyU', 'KeyB', 'KeyZ'];
+  let secret = sec.join('');
+
+  if(cod.length > sec.length ) {
+   cod.splice(0, (cod.length-sec.length))
+  }
+
+  if(enter === secret) {
+    vis.classList.add('visible')
+  }
+};
