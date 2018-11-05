4f0cd5545d6828bacc48781bebc1e014f3dca765
Needle <elena25-2025@ya.ru>
2018-11-05 19:11:48 +0300

    contact-list

---
+++ dom/contact-list/js/js.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var element  = document.getElementById('container');
+var ul = element.querySelector('ul');
+ul.querySelector('li').remove();
+
+var fragment = document.createDocumentFragment();
+var browsers = JSON.parse(loadContacts());
+
+browsers.forEach(function(browser) {
+    let li = document.createElement('li');
+    li.textContent = browser.name;
+    li.setAttribute('data-email', browser.email);
+    li.setAttribute('data-phone', browser.phone);
+    fragment.appendChild(li);
+});
+
+console.log(ul.appendChild(fragment));
+console.log(ul.appendChild(fragment)[0]);
