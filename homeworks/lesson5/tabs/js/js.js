1dd6b9b26f19ad0a1f83f4f7e734477887f380a7
Needle <elena25-2025@ya.ru>
2018-11-07 23:53:47 +0300

    https://repl.it/@vert/HJ-23-Zadacha-2-Taby

---
+++ homeworks/lesson5/tabs/js/js.js
@@ -0,0 +1,33 @@
+'use strict';
+var tabs = document.querySelector('.tabs')
+var email = document.querySelectorAll('nav > a')[0];
+var sms = document.querySelectorAll('nav > a')[1];
+var content = document.getElementById('content');
+var preloader = document.getElementById('preloader');
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "components/email-tab.html",false);
+xhr.addEventListener("progress", () => {
+  preloader.classList.remove('hidden');
+});
+xhr.send();
+email.classList.add('active');
+content.innerHTML = xhr.responseText;
+
+var xhr2 = new XMLHttpRequest();
+xhr2.open("GET", "components/sms-tab.html",false);
+xhr2.send();
+
+email.onclick = function() {
+  this.removeAttribute('href');
+  sms.classList.remove('active');
+  this.classList.add('active');
+  content.innerHTML = xhr.responseText;
+};
+
+sms.onclick = function() {
+  this.removeAttribute('href');
+  email.classList.remove('active');
+  this.classList.add('active');
+  content.innerHTML = xhr2.responseText;
+};
