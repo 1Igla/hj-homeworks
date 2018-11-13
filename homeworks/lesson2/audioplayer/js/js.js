2b899afaf0c3d4be72a8159165a155d3cf3c9542
Needle <elena25-2025@ya.ru>
2018-11-13 22:59:03 +0300

    fixed

--- homeworks/lesson2/audioplayer/js/js.js
+++ homeworks/lesson2/audioplayer/js/js.js
@@ -11,57 +11,61 @@
 
 var play = document.getElementsByClassName('playstate')[0];
 var player = document.getElementsByTagName('audio')[0];
-var togg = document.getElementsByClassName('fa-play')[0];
+var togg = document.getElementsByClassName('playstate')[0];
 var back = document.getElementsByClassName('back')[0];
 var stop = document.getElementsByClassName('stop')[0];
 var next = document.getElementsByClassName('next')[0];
 var title = document.getElementsByClassName('title')[0];
 var stylus = document.getElementsByClassName('stylus')[0];
-
+var mediaplayer = document.getElementsByClassName('mediaplayer')[0];
+var isplaying = false;
 var arrName = ["LA Chill Tour", "This is it band", "LA Fusion Jam"];
 var arrLink = ["https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Chill%20Tour.mp3", "https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This%20is%20it%20band.mp3", "https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA%20Fusion%20Jam.mp3"];
 
 play.onclick = () => {
-
   if ( player.paused ) {
     player.play();
-    togg.classList.toggle('fa-pause');
+    isplaying = true;
+    mediaplayer.classList.add('play');
   } else {
     player.pause();
-   togg.classList.toggle('fa-pause');
-   stylus.classList.toggle('mediaplayer.play stylus');
+    isplaying = false;
+    mediaplayer.classList.remove('play');
+    mediaplayer.classList.add('pause');
   }
-}
+};
 
 stop.onclick = () => {    //stop
   if(!(player.paused)) {
     player.pause();
     player.currentTime = 0;
-  } else {
-    player.play();
   }
-}
+};
 
 var i = arrLink.length;
 var y = arrName.length;
 back.onclick = () => {    //left
   title.title=arrName[--y];
   player.src=arrLink[--i];
-  //player.play();
+if( isplaying ) {
+  player.play();
+}
   if(i==0) {
     i=arrLink.length;
     y=arrName.length;
   }
-}
+};
 
 var k = 0;
 var n = 0;
 next.onclick = () => {   //right
   title.title=arrName[++n];
   player.src=arrLink[++k];
-  //player.play();
-  if(k==arrLink.length-1) {
+   if(k==arrLink.length-1) {
     k=-1;
     n=-1;
+  }
+  if( isplaying ) {
+    player.play();
   }
 }
