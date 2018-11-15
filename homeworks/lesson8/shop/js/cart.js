db5187a013d42c4a0be7034e74013e45940a744c
Needle <elena25-2025@ya.ru>
2018-11-16 01:22:48 +0300

    addToCart(iter(titl))

--- homeworks/lesson8/shop/js/cart.js
+++ homeworks/lesson8/shop/js/cart.js
@@ -4,22 +4,19 @@ const prod = document.querySelectorAll('.add-to-cart');
 const products = Array.from(prod);
 
 
-
-for (const prod of products){
-	prod.addEventListener('click', addToCart);
-  const titl = prod.getAttribute('data-title');
-  console.log(typeof prod);
-}
-
 function iter(atr) {
   for(var i=0; i < items.length; i++ ) {
     if(items[i].title === atr) {
-      console.log(items[i]);
+      return items[i];
     }
   }
 };
-iter(prod.title)
-//iter(items[1].title)
 
-//arr.forEach(e.addEventListener('click', addToCart));
-//addToCart( items[0] ); // кнопка добавления товара в корзину
+
+for (const prod of products){
+  const titl = prod.getAttribute('data-title');
+  prod.addEventListener('click', function () {
+    addToCart(iter(titl));
+    console.log(titl);
+  });
+};
