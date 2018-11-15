d6f782a514b625ec3ee8af70805d0bfea7188353
Needle <elena25-2025@ya.ru>
2018-11-15 22:14:26 +0300

    done

--- homeworks/lesson8/table/js/sort.js
+++ homeworks/lesson8/table/js/sort.js
@@ -1,5 +1,18 @@
 'use strict';
 
 function handleTableClick(event) {
-  
-}
+  const marker = event.target;
+
+   if (marker.tagName === "TH") {
+    const sort = marker.dataset.dir;
+    const table = document.querySelector('table');
+    table.dataset.sortBy = marker.dataset.propName;
+
+     if (sort < 0) {
+      marker.dataset.dir = 1;
+    }else if (sort > 0 || !sort) {
+      marker.dataset.dir = -1;
+    }
+     sortTable(table.dataset.sortBy, sort);
+  }
+};
