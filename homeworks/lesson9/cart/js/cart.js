e7241a78b43e64723cfb2f5bde157b8b244ffa76
Needle <elena25-2025@ya.ru>
2018-11-20 01:19:57 +0300

    fix

--- homeworks/lesson9/cart/js/cart.js
+++ homeworks/lesson9/cart/js/cart.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const colorSwatch = document.getElementById('colorSwatch');
+const sizeSwatch = document.getElementById('sizeSwatch');
+const quickCart = document.getElementById('quick-cart');
+const cartForm = document.getElementById('AddToCartForm');
+const urls = [
+  'https://neto-api.herokuapp.com/cart/colors',
+  'https://neto-api.herokuapp.com/cart/sizes',
+  'https://neto-api.herokuapp.com/cart'
+];
+
+Promise.all(urls.map(url => fetch(url)))
+  .then(resp => Promise.all(resp.map(result => result.json())))
+  .then(([dataColors, dataSizes, dataCart]) => {
+    snippetSwatchColor(dataColors);
+    snippetSwatchSize(dataSizes);
+    snippetCart(dataCart);
+  });
+
+function snippetSwatchColor(data, colorDefault = 'red') {
+  if (!data.length) {
+    return;
+  }
+
+  if (localStorage.selectedColor === undefined) {
+    localStorage.selectedColor = colorDefault;
+  }
+
+  for (let item of data) {
+    const tpl = document.createElement('div');
+    tpl.dataset.value = item.type;
+    tpl.classList.add('swatch-element', 'color', item.type, item.isAvailable ? 'available' : 'soldout');
+    tpl.innerHTML = `
+      <div class="tooltip">${item.title}</div>
+      <input quickbeam="color" id="swatch-1-${item.type}" type="radio" name="color" value="${item.type}" ${item.type === colorDefault ? 'checked' : ''} ${!item.isAvailable ? 'disabled' : ''}>
+      <label for="swatch-1-${item.type}" style="border-color: ${item.code};">
+        <span style="background-color: ${item.code};"></span>
+        <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+      </label>
+    `;
+
+    const radio = tpl.querySelector('input');
+    radio.checked = localStorage.selectedColor === radio.value;
+    radio.addEventListener('change', (event) => {
+      localStorage.selectedColor = event.target.value;
+    });
+
+    colorSwatch.appendChild(tpl);
+  }
+}
--- homeworks/lesson9/sign-in-form/js/auth.js
+++ homeworks/lesson9/sign-in-form/js/auth.js
@@ -1,1 +1,50 @@
 'use strict';
+const formSignin = document.querySelector('.sign-in-htm');
+const formDataIn = new FormData(formSignin);
+
+
+const formSignup = document.querySelector('.sign-up-htm');
+const formDataUp = new FormData(formSignup);
+
+const xhrSignin = new XMLHttpRequest();
+const xhrSignup = new XMLHttpRequest();
+
+const buttonSignin = formSignin.querySelector('.button');
+const buttonSignup = formSignin.querySelector('.button');
+
+xhrSignin.open('POST', 'https://neto-api.herokuapp.com/signin');
+xhrSignup.open('POST', 'https://neto-api.herokuapp.com/signup');
+
+buttonSignin.addEventListener('click', () => {
+  xhrSignin.send(JSON.stringify(formDataIn));
+  xhrSignin.addEventListener('load', () =>{
+    let answer = JSON.parse(xhrSignin.responseText);
+    let outMessage = formSignin.querySelector('.error-message');
+    console.log(answer);
+    console.log('error' in answer);
+    if ('error' in answer) {
+      outMessage.textContent = answer.message;
+    }
+    else{
+      outMessage.textContent = `Пользователь ${answer.name} успешно авторизован`
+    }
+  })
+})
+
+
+
+buttonSignup.addEventListener('click', () => {
+  xhrSignup.send(JSON.stringify(formDataUp));
+  xhrSignup.addEventListener('load', () =>{
+    let answer = JSON.parse(xhrSignup.responseText);
+    let outMessage = formSignup.querySelector('.error-message');
+    console.log(answer);
+    console.log('error' in answer);
+    if ('error' in answer) {
+      outMessage.textContent = answer.message;
+    }
+    else{
+      outMessage.textContent = `Пользователь ${answer.name} успешно зарегестрирован`
+    }
+  })
+})
