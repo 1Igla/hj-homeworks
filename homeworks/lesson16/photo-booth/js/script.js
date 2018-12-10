2fd429559415cb5fa32b484e3c32e0f1e2a3cb5c
Needle <elena25-2025@ya.ru>
2018-12-10 21:51:55 +0300

    takeTemplate, takePicture, stream

--- homeworks/lesson16/photo-booth/js/script.js
+++ homeworks/lesson16/photo-booth/js/script.js
@@ -1,7 +1,59 @@
 'use strict';
-navigator.mediaDevices
-.getUserMedia({video: true, audio: false})
-.then((stream) => {
-const video = document.querySelector('.container');
-video.src = URL.createObjectURL(stream);
-})
+var elem = document.createElement("video");
+elem.setAttribute("autoplay", "autoplay");
+var audio = document.createElement("audio");
+var app = document.querySelector('.app');
+var image = document.createElement("image");
+app.appendChild(image);
+//var body = document.querySelector('body');
+//body.insertBefore(elem, body.firstChild);
+//<audio id="audio" src="http://dev.interactive-creation-works.net/1/1.ogg" ></audio>
+var container = document.querySelector('.container').appendChild(elem);
+var errorText = document.getElementById('error-message');
+var controls = document.querySelector('.controls');
+var takePhoto = document.getElementById('take-photo');
+var video = document.querySelector('video');
+var isplaying = true;
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+var list = document.querySelector('.list');
+
+if(navigator.webkitGetUserMedia!=null) {
+  var options = {
+    video:true,
+    audio:false
+  };
+  // запрашиваем доступ к веб-камере
+    navigator.webkitGetUserMedia(options,
+      function(stream) {
+        controls.style.display = 'block';
+        // включаем поток в URL
+        video.src = window.webkitURL.createObjectURL(stream);
+      },
+      function(e) {
+        errorText.appendChild(document.createTextNode("error happened"));
+        errorText.style.display= 'block';
+        console.log(document.getElementById('error-message'));
+      }
+    );
+  };
+
+takePhoto.addEventListener('click', takePicture);
+function takePicture(stream) {
+  if (isplaying) {
+    video.pause();
+    isplaying = false;
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0);
+    image.setAttribute('src', canvas.toDataURL('image/png'));
+    list.style.visibility = 'inherit';
+    list.insertBefore(image, list.firstChild);
+    console.log(list);
+    takeTemplate(image);
+  }
+  else {
+    video.play();
+    isplaying = true;
+  };
+};
