83af6e2c7e7eeaceb438e6eab9e0cb3cf7518443
Needle <elena25-2025@ya.ru>
2018-12-11 01:18:08 +0300

    upload, dataURItoBlob, handleClick

--- homeworks/lesson16/photo-booth/js/script.js
+++ homeworks/lesson16/photo-booth/js/script.js
@@ -3,11 +3,9 @@ var elem = document.createElement("video");
 elem.setAttribute("autoplay", "autoplay");
 var audio = document.createElement("audio");
 var app = document.querySelector('.app');
-var image = document.createElement("image");
+app.appendChild(video);
 app.appendChild(image);
-//var body = document.querySelector('body');
-//body.insertBefore(elem, body.firstChild);
-//<audio id="audio" src="http://dev.interactive-creation-works.net/1/1.ogg" ></audio>
+
 var container = document.querySelector('.container').appendChild(elem);
 var errorText = document.getElementById('error-message');
 var controls = document.querySelector('.controls');
@@ -38,6 +36,8 @@ if(navigator.webkitGetUserMedia!=null) {
     );
   };
 
+
+
 takePhoto.addEventListener('click', takePicture);
 function takePicture(stream) {
   if (isplaying) {
@@ -46,14 +46,99 @@ function takePicture(stream) {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0);
+
+    var image = document.createElement("image");
     image.setAttribute('src', canvas.toDataURL('image/png'));
     list.style.visibility = 'inherit';
-    list.insertBefore(image, list.firstChild);
+    let pic = takeTemplate(image);
+    list.insertBefore(pic, list.firstChild);
     console.log(list);
-    takeTemplate(image);
+
   }
   else {
     video.play();
     isplaying = true;
   };
+};
+
+function el(tag, attr, children) {
+  const element = document.createElement(tag);
+  if (attr instanceof Object) {
+    Object.keys(attr).forEach(i => {
+      element.setAttribute(i, attr[i])
+    });
+  }
+  if (typeof children === 'string') {
+    element.textContent = children;
+  } else if (children instanceof Array) {
+    children.forEach(child => element.appendChild(child));
+  }
+  return element;
+};
+
+function takeTemplate(image){
+  return el('figure', null, [
+    el('img', {src: img}),
+    el('figcaption', null, [
+      el('a', {href: img, download: 'snapshot.png'}, [
+        el('i', {class: 'material-icons'}, 'file_download')
+      ]),
+      el('a', null, [
+        el('i', {class: 'material-icons'}, 'file_upload')
+      ]),
+      el('a', null, [
+        el('i', {class: 'material-icons'}, 'delete')
+      ])
+    ])
+  ])
+};
+
+function upload(img) {
+  var formData = new FormData();
+  var blob = dataURItoBlob(img.src);
+  formData.append('image', blob);
+  return fetch('https://neto-api.herokuapp.com/photo-booth', {
+    method: 'POST',
+    body: data
+  });
+};
+
+function dataURItoBlob(dataURI) {
+const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+const array = [];
+const byteString = atob(dataURI.split(',')[1]);
+
+for(let i = 0; i < byteString.length; i++) {
+  array.push(byteString.charCodeAt(i));
+}
+
+return new Blob([new Uint8Array(array)], { type: mimeString });
+}
+
+function handleClick(event) {
+var btnName = event.target.innerText;
+var figure = event.target.closest('figure');
+if (btnName !== 'file_download') {
+  event.preventDefault();
+  switch (btnName) {
+    case 'file_upload' :
+      const img = figure.querySelector('img');
+      upload(img)
+        .then(res => {
+        if (200 <= res.status && res.status < 300) {
+          event.target.parentNode.style.visibility = 'hidden';
+        }
+      })
+        .catch(err => {
+          error.style.display = 'block';
+          error.textContent = err;
+        });
+      break;
+    case 'delete' :
+      figure.remove();
+      break;
+    }
+  } else {
+    event.target.parentNode.style.visibility = 'hidden';
+  }
 };
