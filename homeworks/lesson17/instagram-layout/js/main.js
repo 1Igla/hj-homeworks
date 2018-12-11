495856d60451f8f7756b49b9c86ead36e609c588
Needle <elena25-2025@ya.ru>
2018-12-11 23:55:12 +0300

    inst

--- homeworks/lesson17/instagram-layout/js/main.js
+++ homeworks/lesson17/instagram-layout/js/main.js
@@ -15,7 +15,54 @@ class iLayout {
     this.registerEvents();
   }
   registerEvents() {
+   for(var el of this.positionsContainer.children){
+     el.addEventListener('dragover', this.showActive.bind(this));
+     el.addEventListener('drop', this.loadImage.bind(this));
+     el.addEventListener('dragleave', this.hideActive.bind(this));
+    }
+   this.actionButton.addEventListener('click', this.collage.bind(this));
+   }
+  showActive(event) {
+    event.preventDefault();
+    event.target.classList.add('layout__item_active');
   }
+  hideActive(event) {
+    event.target.classList.remove('layout__item_active');
+  }
+  loadImage(event) {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    if (files[0].type.includes('image')) {
+      this.hideActive(event);
+      const img = document.createElement('img');
+      img.src = URL.createObjectURL(files[0]);
+      img.classList.add('layout__image');
+      event.target.appendChild(img);
+      this.setLayout(event.target);
+    } else {
+      alert('Фаил не изображение');
+    }
+  }
+  collage() {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.height = this.positionsContainer.offsetHeight;
+    canvas.width = this.positionsContainer.offsetWidth;
+    for(var el of this.positionsContainer.children){
+      const image = el.getElementsByTagName('img')[0];
+      if (image) {
+        context.drawImage(image,0,0,
+          image.naturalWidth,Math.min(image.naturalHeight, image.naturalWidth / el.offsetWidth * el.offsetHeight),
+          el.offsetLeft - el.parentElement.offsetLeft,el.offsetTop - el.parentElement.offsetTop,
+          layout.offsetWidth,Math.min(el.offsetHeight, image.naturalHeight / image.naturalWidth * el.offsetWidth)
+          );
+      }
+    }
+      const collage = document.createElement('img');
+      collage.src = canvas.toDataURL();
+      this.positionsContainer.innerHTML = '';
+      this.positionsContainer.appendChild(collage);
+      this.result.value = `<img src=${canvas.toDataURL()}>`;
+   }
 }
-
 new iLayout( document.getElementById( 'layout' ));
