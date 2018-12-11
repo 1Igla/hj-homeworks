d610af63bcd748635612a9fc05bf30b15b335514
Needle <elena25-2025@ya.ru>
2018-12-11 22:48:47 +0300

    fix text-editor, sprite-generator

--- homeworks/lesson17/sprite-generator/js/main.js
+++ homeworks/lesson17/sprite-generator/js/main.js
@@ -3,19 +3,38 @@ const prop = ( data, name ) => data.map( item => item[ name ] ),
 class SpriteGenerator {
   constructor( container ) {
     this.uploadButton = container.querySelector( '.sprite-generator__upload' );
-
     this.submitButton = container.querySelector( '.sprite-generator__generate' );
     this.imagesCountContainer = container.querySelector( '.images__added-count-value' );
     this.codeContainer = container.querySelector( '.sprite-generator__code' );
     this.imageElement = container.querySelector( '.sprite-generator__result-image' );
     this.images = [];
-
     this.imagesCount = 0;
-
     this.registerEvents();
   }
+
   registerEvents() {
+    this.uploadButton.addEventListener('change', onSelectFiles);
   }
+   onSelectFiles(event) {
+     const files = Array.from(event.target.files);
+     updateFilesInfo(files);
+   }
+
+
+updateFilesInfo(files) {
+    const imageTypeRegExp = /^image\//;
+    if (imageTypeRegExp.test(file.type)) {
+  		 const img = document.createElement('img');
+       img.width = 150;
+       img.height = 150;
+       img.src = window.URL.createObjectURL(file);
+       img.addEventListener('load', event => {
+           window.URL.revokeObjectURL(event.target.src);
+          });
+     this.imageElement.appendChild(img);
+     console.log(img)
+     }
+   }
 }
 
 new SpriteGenerator( document.getElementById( 'generator' ));
--- homeworks/lesson17/text-editor/js/main.js
+++ homeworks/lesson17/text-editor/js/main.js
@@ -9,48 +9,47 @@ class TextEditor {
   constructor( container, storageKey = '_text-editor__content' ) {
     this.container = container;
     this.contentContainer = container.querySelector( '.text-editor__content' );
-  //  this.contentContainer.setAttribute('draggable', true);
     this.hintContainer = container.querySelector( '.text-editor__hint' );
     this.filenameContainer = container.querySelector( '.text-editor__filename' );
     this.storageKey = storageKey;
     this.registerEvents();
-    console.log(this);
     this.load( this.getStorageData());
   }
   registerEvents() { //Подключение обработчиков событий
     const save = throttle( this.save.bind( this ), 1000 );
     const loadFile = this.loadFile.bind( this );
     const showHint = this.showHint.bind( this );
-    const hideHint = this.hideHint.bind( this );
-
-    //this.contentContainer.addEventListener( 'dragover', showHint);
-    this.contentContainer.addEventListener( 'drop', loadFile );
+    
     this.contentContainer.addEventListener( 'input', save );
-  //  this.contentContainer.addEventListener( 'dragleave', hideHint);
+    this.container.addEventListener( 'dragover', showHint);
+    this.container.addEventListener( 'drop', loadFile );
+    
 
   }
   loadFile(e) { //для загрузки файла после переноса в окно редактора
     e.preventDefault();
     const files = Array.from(e.dataTransfer.files);
-    this.setFilename( files[0].name );
-    //this.hideHint(e);
-    this.readFile(files[0]);
-
-  }
-  readFile( file ) { //для чтения .txt файла
     const textTypeRegExp = /^text\//;
-    if (textTypeRegExp.test(file.type)) {
-      const reader = new FileReader();
-      this.contentContainer.value = '';
-      reader.addEventListener('load', event => {
-      this.contentContainer.value = event.target.result;
-      });
-     reader.readAsText(file);
-      }
+    if (textTypeRegExp.test(files[0].type)) {
+      this.hideHint();
+      this.setFilename(files[0].name);
+      this.readFile(files[0]);
+    }
     else {
       alert('Wrong extension by file');
+      this.hideHint();
     }
   }
+
+  readFile( file ) { //для чтения .txt файла
+    const reader = new FileReader();
+    this.contentContainer.value = '';
+    reader.addEventListener('load', event => {
+      this.contentContainer.value = event.target.result;
+    });
+    reader.readAsText(file);
+  }
+    
   setFilename( filename ) {// для установки имени файла
     this.filenameContainer.textContent = filename;
   }
@@ -59,7 +58,6 @@ class TextEditor {
     this.hintContainer.classList.add('text-editor__hint_visible');
   }
   hideHint(e) { // для скрытия подсказки
-    e.preventDefault();
     this.hintContainer.classList.remove('text-editor__hint_visible');
   }
   load( value ) {
@@ -71,6 +69,6 @@ class TextEditor {
   save() {
     localStorage[ this.storageKey ] = this.contentContainer.value;
   }
-}
+};
 
 new TextEditor( document.getElementById( 'editor' ));
