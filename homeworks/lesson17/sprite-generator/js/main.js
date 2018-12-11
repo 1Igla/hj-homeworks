cc44798128333362da9cb39dcbd26a06ab815c33
Needle <elena25-2025@ya.ru>
2018-12-11 23:45:34 +0300

    needs to improve

--- homeworks/lesson17/sprite-generator/js/main.js
+++ homeworks/lesson17/sprite-generator/js/main.js
@@ -1,5 +1,6 @@
 const prop = ( data, name ) => data.map( item => item[ name ] ),
   summ = data => data.reduce(( total, value ) => total + value, 0 );
+
 class SpriteGenerator {
   constructor( container ) {
     this.uploadButton = container.querySelector( '.sprite-generator__upload' );
@@ -13,28 +14,58 @@ class SpriteGenerator {
   }
 
   registerEvents() {
+    const onSelectFiles = this.onSelectFiles.bind( this );
+    const generateSprite = this.generateSprite.bind(this);
+
     this.uploadButton.addEventListener('change', onSelectFiles);
+    this.submitButton.addEventListener('click', generateSprite);
   }
-   onSelectFiles(event) {
+
+  onSelectFiles(event) {
      const files = Array.from(event.target.files);
-     updateFilesInfo(files);
+     this.updateFilesInfo(files);
    }
 
-
-updateFilesInfo(files) {
+   updateFilesInfo(file) {
     const imageTypeRegExp = /^image\//;
     if (imageTypeRegExp.test(file.type)) {
   		 const img = document.createElement('img');
-       img.width = 150;
-       img.height = 150;
        img.src = window.URL.createObjectURL(file);
        img.addEventListener('load', event => {
            window.URL.revokeObjectURL(event.target.src);
           });
-     this.imageElement.appendChild(img);
-     console.log(img)
+       this.imageElement.appendChild(img);
+       console.log(img);
+       var imgs;
+      this.images.push(imgs);
+      this.setImageCount();
      }
    }
-}
+
+  setImageCount() {
+    this.imagesCount = this.images.length;
+    this.imagesCountContainer.innerText = this.imagesCount;
+  }
+
+  generateSprite(event) {
+    event.preventDefault();
+    var width = Math.ceil(Math.sqrt(this.imagesCount));
+    var height = Math.ceil(this.imagesCount / width);
+    var maxWidth = 0;
+    var maxHeight = 0;
+    for (var image of this.images) {
+      if (image.image.naturalHeight > maxHeight) {
+        maxHeight = image.image.naturalHeight;
+      }
+      if (image.image.naturalWidth > maxWidth) {
+        maxWidth = image.image.naturalWidth;
+      }
+    }
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    this.imageElement.src = canvas.toDataURL('image/jpeg');
+  }
+};
 
 new SpriteGenerator( document.getElementById( 'generator' ));
